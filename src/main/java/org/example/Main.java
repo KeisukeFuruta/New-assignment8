df8c2f847735074fcda01ebb54040557ed531444
KeisukeFuruta <kcomethunter@gmail.com>
2024-06-09 22:13:01 +0900

    コメント文の位置を調整

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,8 @@ import java.nio.file.StandardOpenOption;
 public class Main {
 
   public static void main(String[] args) {
+    //課題　入出力を実際に実装して、作ったファイルに文章を追加する。
+    //その作ったファイルを削除してもう一度実行すると例外になるので、その例外をキャッチして、例外処理を行う
     try {
       Path path = Path.of("JavaCourse.txt");
       Files.writeString(path, "Javaコース楽しい！" + " "
@@ -17,6 +19,7 @@ public class Main {
       //e.printStackTrace(); //例外の処理をコンソール上に表示させる
     } catch (IOException e1) {
       System.out.println("例外が発生しました、該当ファイルがあるか確認してください。");
+      // catchした中で更にファイルに書き込もうとして例外になった場合、どうなるのかを確認してください。
       try {
         Path path = Path.of("JavaCourse.txt");
         Files.writeString(path, "Javaコース楽しい！" + " "
@@ -25,14 +28,7 @@ public class Main {
         //e.printStackTrace(); //例外の処理をコンソール上に表示させる
       } catch (IOException e2) {
         System.out.println("もう一度例外が発生しました、該当ファイルがあるか確認してください。");
-
       }
     }
-    //
-    //課題　入出力を実際に実装して、作ったファイルに文章を追加する。
-    //その作ったファイルを削除してもう一度実行すると例外になるので、その例外をキャッチして、例外処理を行う
-
-    // catchした中で更にファイルに書き込もうとして例外になった場合、どうなるのかを確認してください。
-
   }
 }
